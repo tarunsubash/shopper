9908db34bd6c169dccb500f41c7cda778ac20860
Subash <subash@Subashs-MacBook-Pro.local>
2019-12-23 23:06:03 +0530

    add model classes

---
+++ shopper/ProductList/Models/ProductModel.swift
@@ -0,0 +1,15 @@
+//
+//  ProductModel.swift
+//  shopper
+//
+//  Created by Subash on 23/12/19.
+//  Copyright © 2019 Subash. All rights reserved.
+//
+
+import Foundation
+
+struct Product: Codable {
+    let name: String
+    let price: String
+    let image: String
+}
